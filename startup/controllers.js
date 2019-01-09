3a73a94b567ca9ad174dcb12851c41c8d8275d3c
Shirak <shirakavakian@gmail.com>
2019-01-09 15:34:19 -0800

    allow x-auth-token

--- startup/controllers.js
+++ startup/controllers.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Headers', 'x-auth-token,content-type');
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
         res.setHeader('Access-Control-Allow-Credentials', true);
